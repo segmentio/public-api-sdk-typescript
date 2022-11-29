6e690c6c392764ad2410cf2989d62940d81c0be4
public-api ci <team-apics@twilio.com>
2022-11-29 18:48:12 +0000

    Automated client deploy (from segmentio/public-api@42854ef)

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     roots: ['<rootDir>/tests'],
     testTimeout: 1000,
-    testRegex: 'tests/(.+)\\.test\\.(jsx?|tsx?)$',
+    testRegex: '(.*)test\\.(js|jsx?|tsx?)$',
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
     testEnvironment: 'node',
+    cacheDirectory: './node_modules/.cache/jest',
+    verbose: true,
+    silent: false,
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
