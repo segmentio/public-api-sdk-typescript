cd83565f99b6bd335cd53239b95f8ddce85c5628
APIs and Common Services team <team-apics@twilio.com>
2023-03-15 14:05:10 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@4c22645.

--- dist/api/profilesSyncApi.js
+++ dist/api/profilesSyncApi.js
@@ -202,6 +202,95 @@ var ProfilesSyncApi = (function () {
             });
         });
     };
+    ProfilesSyncApi.prototype.removeProfilesWarehouseFromSpace = function (spaceId, warehouseId, options) {
+        if (options === void 0) { options = { headers: {} }; }
+        return __awaiter(this, void 0, void 0, function () {
+            var localVarPath, localVarQueryParameters, localVarHeaderParams, produces, localVarFormParams, localVarUseFormData, localVarRequestOptions, authenticationPromise, interceptorPromise, _loop_2, _i, _a, interceptor;
+            var _this = this;
+            return __generator(this, function (_b) {
+                localVarPath = this.basePath +
+                    '/spaces/{spaceId}/profiles-warehouses/{warehouseId}'
+                        .replace('{' + 'spaceId' + '}', encodeURIComponent(String(spaceId)))
+                        .replace('{' + 'warehouseId' + '}', encodeURIComponent(String(warehouseId)));
+                localVarQueryParameters = {};
+                localVarHeaderParams = Object.assign({}, this._defaultHeaders);
+                produces = [
+                    'application/vnd.segment.v1alpha+json',
+                    'application/json',
+                ];
+                if (produces.indexOf('application/json') >= 0) {
+                    localVarHeaderParams.Accept = 'application/json';
+                }
+                else {
+                    localVarHeaderParams.Accept = produces.join(',');
+                }
+                localVarFormParams = {};
+                if (spaceId === null || spaceId === undefined) {
+                    throw new Error('Required parameter spaceId was null or undefined when calling removeProfilesWarehouseFromSpace.');
+                }
+                if (warehouseId === null || warehouseId === undefined) {
+                    throw new Error('Required parameter warehouseId was null or undefined when calling removeProfilesWarehouseFromSpace.');
+                }
+                Object.assign(localVarHeaderParams, options.headers);
+                localVarUseFormData = false;
+                localVarRequestOptions = {
+                    method: 'DELETE',
+                    qs: localVarQueryParameters,
+                    headers: localVarHeaderParams,
+                    uri: localVarPath,
+                    useQuerystring: this._useQuerystring,
+                    json: true,
+                };
+                authenticationPromise = Promise.resolve();
+                if (this.authentications.token.accessToken) {
+                    authenticationPromise = authenticationPromise.then(function () {
+                        return _this.authentications.token.applyToRequest(localVarRequestOptions);
+                    });
+                }
+                authenticationPromise = authenticationPromise.then(function () {
+                    return _this.authentications.default.applyToRequest(localVarRequestOptions);
+                });
+                interceptorPromise = authenticationPromise;
+                _loop_2 = function (interceptor) {
+                    interceptorPromise = interceptorPromise.then(function () {
+                        return interceptor(localVarRequestOptions);
+                    });
+                };
+                for (_i = 0, _a = this.interceptors; _i < _a.length; _i++) {
+                    interceptor = _a[_i];
+                    _loop_2(interceptor);
+                }
+                return [2, interceptorPromise.then(function () {
+                        if (Object.keys(localVarFormParams).length) {
+                            if (localVarUseFormData) {
+                                localVarRequestOptions.formData = localVarFormParams;
+                            }
+                            else {
+                                localVarRequestOptions.form = localVarFormParams;
+                            }
+                        }
+                        return new Promise(function (resolve, reject) {
+                            (0, request_1.default)(localVarRequestOptions, function (error, response, body) {
+                                if (error) {
+                                    reject(error);
+                                }
+                                else {
+                                    if (response.statusCode &&
+                                        response.statusCode >= 200 &&
+                                        response.statusCode <= 299) {
+                                        body = models_1.ObjectSerializer.deserialize(body, 'RemoveProfilesWarehouseFromSpace200Response');
+                                        resolve({ response: response, body: body });
+                                    }
+                                    else {
+                                        reject(new apis_1.HttpError(response, body, response.statusCode));
+                                    }
+                                }
+                            });
+                        });
+                    })];
+            });
+        });
+    };
     return ProfilesSyncApi;
 }());
 exports.ProfilesSyncApi = ProfilesSyncApi;
