45cccf8cdbf79d2bc813f9c12c054e27652afade
APIs and Common Services team <team-apics@twilio.com>
2023-08-24 17:04:17 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@cbbca23.

--- api/eventsApi.ts
+++ api/eventsApi.ts
@@ -107,7 +107,7 @@ export class EventsApi {
     }
 
     /**
-     * Enumerates the Workspace event volumes over time in minute increments.   The rate limit for this endpoint is 20 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.
+     * Enumerates the Workspace event volumes over time in minute increments.   The rate limit for this endpoint is 60 requests per minute, which is lower than the default due to access pattern restrictions. Once reached, this endpoint will respond with the 429 HTTP status code with headers indicating the limit parameters. See [Rate Limiting](/#tag/Rate-Limits) for more information.
      * @summary Get Events Volume from Workspace
      * @param granularity The size of each bucket in the requested window.  This parameter exists in v1.
      * @param startTime The ISO8601 formatted timestamp that corresponds to the beginning of the requested time frame, inclusive.  This parameter exists in v1.
