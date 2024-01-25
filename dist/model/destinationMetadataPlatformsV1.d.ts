c48a8a248ddcf092ef18c17d8d2a479cf73905fc
APIs and Common Services team <team-apics@twilio.com>
2024-01-25 18:12:22 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@680a66ed.

--- dist/model/destinationMetadataPlatformsV1.d.ts
+++ dist/model/destinationMetadataPlatformsV1.d.ts
@@ -2,6 +2,8 @@ export declare class DestinationMetadataPlatformsV1 {
     'browser'?: boolean;
     'server'?: boolean;
     'mobile'?: boolean;
+    'warehouse'?: boolean;
+    'cloudAppObject'?: boolean;
     static discriminator: string | undefined;
     static attributeTypeMap: Array<{
         name: string;
