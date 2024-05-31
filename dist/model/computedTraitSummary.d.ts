d01924bc7835d3a20c8e94f1f3ba1be3eb55f490
APIs and Common Services team <team-apics@twilio.com>
2024-05-31 19:33:52 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@043d4b9e.

--- dist/model/computedTraitSummary.d.ts
+++ dist/model/computedTraitSummary.d.ts
@@ -1,4 +1,4 @@
-import { Definition } from './definition';
+import { Definition1 } from './definition1';
 import { TraitOptions } from './traitOptions';
 export declare class ComputedTraitSummary {
     'id': string;
@@ -7,7 +7,7 @@ export declare class ComputedTraitSummary {
     'description'?: string;
     'key': string;
     'enabled': boolean;
-    'definition': Definition | null;
+    'definition': Definition1 | null;
     'status'?: string;
     'createdBy': string;
     'updatedBy': string;
