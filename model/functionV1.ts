f0619487f11b8cbb1afd8370d9ad69d6ffd200ef
APIs and Common Services team <team-apics@twilio.com>
2024-06-13 05:25:24 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@9dcca2a2.

--- model/functionV1.ts
+++ model/functionV1.ts
@@ -162,6 +162,7 @@ export namespace FunctionV1 {
     export enum ResourceTypeEnum {
         DESTINATION = <any>'DESTINATION',
         INSERT_DESTINATION = <any>'INSERT_DESTINATION',
+        INSERT_SOURCE = <any>'INSERT_SOURCE',
         SOURCE = <any>'SOURCE',
     }
 }
