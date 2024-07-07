e2407f8f624b0f6744a5f90a035dbc350032c726
APIs and Common Services team <team-apics@twilio.com>
2024-07-07 00:09:52 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@7296bdc1.

--- dist/model/computedTraitSummary.js
+++ dist/model/computedTraitSummary.js
@@ -42,7 +42,7 @@ var ComputedTraitSummary = (function () {
         {
             name: 'definition',
             baseName: 'definition',
-            type: 'Definition',
+            type: 'Definition1',
         },
         {
             name: 'status',
