e2bf50c61ed14280617e0fe1b44e807699bad1b9
APIs and Common Services team <team-apics@twilio.com>
2025-04-28 14:59:17 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@300dd1ad.

--- dist/model/getAudienceAlphaOutput.js
+++ dist/model/getAudienceAlphaOutput.js
@@ -12,7 +12,7 @@ var GetAudienceAlphaOutput = (function () {
         {
             name: 'audience',
             baseName: 'audience',
-            type: 'AudienceSummaryWithAudienceTypeAndLookback',
+            type: 'AudienceSummary',
         },
     ];
     return GetAudienceAlphaOutput;
