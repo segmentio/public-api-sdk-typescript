9a3cdb145a399501327f3741af89caeec1f1eb41
APIs and Common Services team <team-apics@twilio.com>
2025-09-19 19:01:15 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@115ac15e.

---
+++ dist/model/addAudienceScheduleToAudienceAlphaOutput.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AddAudienceScheduleToAudienceAlphaOutput = void 0;
+var AddAudienceScheduleToAudienceAlphaOutput = (function () {
+    function AddAudienceScheduleToAudienceAlphaOutput() {
+    }
+    AddAudienceScheduleToAudienceAlphaOutput.getAttributeTypeMap = function () {
+        return AddAudienceScheduleToAudienceAlphaOutput.attributeTypeMap;
+    };
+    AddAudienceScheduleToAudienceAlphaOutput.discriminator = undefined;
+    AddAudienceScheduleToAudienceAlphaOutput.attributeTypeMap = [
+        {
+            name: 'audienceSchedule',
+            baseName: 'audienceSchedule',
+            type: 'AudienceSchedule',
+        },
+    ];
+    return AddAudienceScheduleToAudienceAlphaOutput;
+}());
+exports.AddAudienceScheduleToAudienceAlphaOutput = AddAudienceScheduleToAudienceAlphaOutput;
+//# sourceMappingURL=addAudienceScheduleToAudienceAlphaOutput.js.map
