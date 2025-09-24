ff743c7d1c6405fa33af1db8e7dd12ce7ae10d91
APIs and Common Services team <team-apics@twilio.com>
2025-09-24 20:01:08 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@cf5ca4e8.

--- api/audiencesApi.ts
+++ api/audiencesApi.ts
@@ -1655,7 +1655,7 @@ export class AudiencesApi {
         });
     }
     /**
-     * Updates an audience schedule for the given audience id.  • This endpoint is in **Alpha** testing.  Please submit any feedback by sending an email to friends@segment.com.   • In order to successfully call this endpoint, the specified Workspace needs to have the Audience feature enabled. Please reach out to your customer success manager for more information.
+     * Updates an audience schedule for a Linked Audience (audienceType = LINKED).  • This endpoint is in **Alpha** testing.  Please submit any feedback by sending an email to friends@segment.com.   • In order to successfully call this endpoint, the specified Workspace needs to have the Audience feature enabled. Please reach out to your customer success manager for more information.
      * @summary Update Audience Schedule for Audience
      * @param spaceId
      * @param id
