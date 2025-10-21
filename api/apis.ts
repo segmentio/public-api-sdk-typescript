f73413f7b6e3801fb14b96b2c48d2b4e7b5f5e8e
APIs and Common Services team <team-apics@twilio.com>
2025-10-21 23:48:39 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@e48039a4.

--- api/apis.ts
+++ api/apis.ts
@@ -171,7 +171,7 @@ export function configureApis(
 
     for (const k of Object.keys(apis)) {
         const key = k as keyof typeof apis;
-        headers['User-Agent'] = 'Public API SDK 61.0.2 (TypeScript)';
+        headers['User-Agent'] = 'Public API SDK 61.1.3 (TypeScript)';
         apis[key].accessToken = token;
         apis[key].defaultHeaders = headers;
     }
@@ -218,7 +218,7 @@ export function configureEuApis(
 
     for (const k of Object.keys(apis)) {
         const key = k as keyof typeof apis;
-        headers['User-Agent'] = 'Public API SDK 61.0.2 (TypeScript)';
+        headers['User-Agent'] = 'Public API SDK 61.1.3 (TypeScript)';
         apis[key].accessToken = token;
         apis[key].defaultHeaders = headers;
     }
