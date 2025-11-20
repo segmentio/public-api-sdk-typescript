88e3103078398cb3171501974b47a295475ef1cf
APIs and Common Services team <team-apics@twilio.com>
2025-11-20 19:26:21 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@29ecbb24.

--- dist/model/createFunctionV1Input.js
+++ dist/model/createFunctionV1Input.js
@@ -49,7 +49,6 @@ exports.CreateFunctionV1Input = CreateFunctionV1Input;
         ResourceTypeEnum[ResourceTypeEnum["DESTINATION"] = 'DESTINATION'] = "DESTINATION";
         ResourceTypeEnum[ResourceTypeEnum["INSERT_DESTINATION"] = 'INSERT_DESTINATION'] = "INSERT_DESTINATION";
         ResourceTypeEnum[ResourceTypeEnum["INSERT_SOURCE"] = 'INSERT_SOURCE'] = "INSERT_SOURCE";
-        ResourceTypeEnum[ResourceTypeEnum["INSERT_TRANSFORMATION"] = 'INSERT_TRANSFORMATION'] = "INSERT_TRANSFORMATION";
         ResourceTypeEnum[ResourceTypeEnum["SOURCE"] = 'SOURCE'] = "SOURCE";
     })(ResourceTypeEnum = CreateFunctionV1Input.ResourceTypeEnum || (CreateFunctionV1Input.ResourceTypeEnum = {}));
 })(CreateFunctionV1Input = exports.CreateFunctionV1Input || (exports.CreateFunctionV1Input = {}));
