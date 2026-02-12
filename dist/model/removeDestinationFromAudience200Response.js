bcc09ef80831bbfb42ce09825db0fde81e375c9b
APIs and Common Services team <team-apics@twilio.com>
2026-02-12 22:42:47 +0000

    Automated SDK update
    This updates the SDK from internal repo commit segmentio/public-api@478a3c7e.

---
+++ dist/model/removeDestinationFromAudience200Response.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RemoveDestinationFromAudience200Response = void 0;
+var RemoveDestinationFromAudience200Response = (function () {
+    function RemoveDestinationFromAudience200Response() {
+    }
+    RemoveDestinationFromAudience200Response.getAttributeTypeMap = function () {
+        return RemoveDestinationFromAudience200Response.attributeTypeMap;
+    };
+    RemoveDestinationFromAudience200Response.discriminator = undefined;
+    RemoveDestinationFromAudience200Response.attributeTypeMap = [
+        {
+            name: 'data',
+            baseName: 'data',
+            type: 'RemoveDestinationFromAudienceAlphaOutput',
+        },
+    ];
+    return RemoveDestinationFromAudience200Response;
+}());
+exports.RemoveDestinationFromAudience200Response = RemoveDestinationFromAudience200Response;
+//# sourceMappingURL=removeDestinationFromAudience200Response.js.map
